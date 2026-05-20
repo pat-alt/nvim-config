df7b79cdaa8f96d16ae390b8ed3268987ca354db
Patrick Altmeyer <paltmeyer@hbk.com>
2026-05-20 08:47:25 -0500

    removed the glossary comments stuff

--- lua/custom/plugins/todo-comments.lua
+++ lua/custom/plugins/todo-comments.lua
@@ -2,8 +2,8 @@ return {
   "folke/todo-comments.nvim",
   dependencies = { "nvim-lua/plenary.nvim" },
   opts = {
-    keywords = {
-      GLOSSARY = { icon = " ", color = "hint", alt = { "GLOSS" } },
-    },
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
   }
 }
