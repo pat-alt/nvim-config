1b362f7e1a4bf170ae5e111ea8b000fd3a74202a
Patrick Altmeyer <paltmeyer@hbk.com>
2026-07-21 09:42:15 -0500

    first attmept

--- lua/custom/plugins/opencode.lua
+++ lua/custom/plugins/opencode.lua
@@ -1,93 +1,68 @@
 return {
-  'nickjvandyke/opencode.nvim',
-  version = '*',
-  dependencies = {
-    {
-      'folke/snacks.nvim',
-      opts = {
-        input = {},
-        picker = {
-          actions = {
-            opencode_send = function(...)
-              return require('opencode').snacks_picker_send(...)
-            end,
-          },
-          win = {
-            input = {
-              keys = {
-                ['<a-a>'] = { 'opencode_send', mode = { 'n', 'i' } },
-              },
-            },
-          },
-        },
-        terminal = {},
-      },
+  'carlos-algms/agentic.nvim',
+  opts = {
+    provider = 'opencode-acp',
+    windows = {
+      position = 'right',
+      width = '40%',
     },
   },
-  config = function()
-    ---@type opencode.Opts
-    vim.g.opencode_opts = {}
-    local curl_supports_fail_with_body = vim.system({ 'curl', '--fail-with-body', '--version' }):wait().code == 0
-    local pixicurl = vim.fn.expand('~/.pixi/bin/pixicurl')
-    if not curl_supports_fail_with_body and vim.fn.executable(pixicurl) == 1 and vim.fn.has('unix') == 1 then
-      local shim_dir = vim.fn.stdpath('cache') .. '/opencode-curl'
-      local curl = shim_dir .. '/curl'
-      vim.fn.mkdir(shim_dir, 'p')
-      if vim.uv.fs_readlink(curl) ~= pixicurl then
-        vim.fn.delete(curl)
-        vim.uv.fs_symlink(pixicurl, curl)
-      end
-      vim.env.PATH = shim_dir .. ':' .. vim.env.PATH
-    end
-
-    vim.o.autoread = true
-
-    local opencode_cmd = 'opencode --port'
-    local snacks_terminal_opts = {
-      win = {
-        position = 'right',
-        enter = false,
-        on_win = function(self)
-          self.opts.wo.winfixwidth = false
-          vim.wo[self.win].winfixwidth = false
-        end,
-      },
-    }
-
-    vim.keymap.set({ 'n', 't' }, '<leader>ao', function()
-      require('snacks.terminal').toggle(opencode_cmd, snacks_terminal_opts)
-    end, { desc = 'OpenCode toggle' })
-
-    vim.keymap.set({ 'n', 'x' }, '<leader>aO', function()
-      require('opencode').ask('@this: ', { submit = true })
-    end, { desc = 'OpenCode ask with context' })
-
-    vim.keymap.set({ 'n', 'x' }, '<leader>ab', function()
-      require('opencode').ask('@buffer: ')
-    end, { desc = 'OpenCode ask current buffer' })
-
-    vim.keymap.set({ 'n', 'x' }, '<leader>af', function()
-      require('opencode').ask('@quickfix: ')
-    end, { desc = 'OpenCode ask quickfix list' })
-
-    vim.keymap.set({ 'n', 'x' }, '<leader>ax', function()
-      require('opencode').select()
-    end, { desc = 'OpenCode actions' })
-
-    vim.keymap.set('n', '<leader>an', function()
-      require('opencode').command('session.new')
-    end, { desc = 'OpenCode new session' })
-
-    vim.keymap.set('n', '<leader>aq', function()
-      require('opencode').command('session.interrupt')
-    end, { desc = 'OpenCode interrupt' })
-
-    vim.keymap.set({ 'n', 'x' }, 'go', function()
-      return require('opencode').operator('@this ')
-    end, { desc = 'Add range to OpenCode', expr = true })
-
-    vim.keymap.set('n', 'goo', function()
-      return require('opencode').operator('@this ') .. '_'
-    end, { desc = 'Add line to OpenCode', expr = true })
-  end,
+  keys = {
+    {
+      '<leader>ao',
+      function()
+        require('agentic').toggle({ auto_add_to_context = false })
+      end,
+      mode = { 'n', 'v', 'i' },
+      desc = 'Agentic toggle',
+    },
+    {
+      '<leader>aO',
+      function()
+        require('agentic').open({ auto_add_to_context = true })
+      end,
+      mode = { 'n', 'v' },
+      desc = 'Agentic open with context',
+    },
+    {
+      '<leader>ab',
+      function()
+        require('agentic').add_file()
+      end,
+      mode = { 'n', 'v' },
+      desc = 'Agentic add current file',
+    },
+    {
+      '<leader>af',
+      function()
+        require('agentic').add_buffer_diagnostics()
+      end,
+      mode = { 'n' },
+      desc = 'Agentic add buffer diagnostics',
+    },
+    {
+      '<leader>an',
+      function()
+        require('agentic').new_session({ auto_add_to_context = false })
+      end,
+      mode = { 'n', 'v' },
+      desc = 'Agentic new session',
+    },
+    {
+      '<leader>aq',
+      function()
+        require('agentic').stop_generation()
+      end,
+      mode = { 'n', 'v', 'i' },
+      desc = 'Agentic stop generation',
+    },
+    {
+      '<leader>ar',
+      function()
+        require('agentic').restore_session()
+      end,
+      mode = { 'n', 'v', 'i' },
+      desc = 'Agentic restore session',
+    },
+  },
 }
